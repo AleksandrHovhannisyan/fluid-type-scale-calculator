9fd370247d00e8b1a6c1476b4a9df0b8cdbef312
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-12-24 09:26:30 -0500

    Use targeted SEO

--- src/_data/site.js
+++ src/_data/site.js
@@ -11,8 +11,8 @@ module.exports = {
   title: 'Fluid Type Scale Generator',
   author: 'Aleksandr Hovhannisyan',
   email: 'aleksandrhovhannisyan@gmail.com',
-  description: 'Generate fluid typography variables with a modular type scale.',
-  keywords: ['fluid typography', 'modular scale', 'type scale'],
+  description: 'Generate fluid type scale CSS variables for use in any project.',
+  keywords: ['fluid type scale', 'modular scale', 'type scale'],
   language: 'en-US',
   favicon: {
     widths: [32, 57, 76, 96, 128, 192, 228],
