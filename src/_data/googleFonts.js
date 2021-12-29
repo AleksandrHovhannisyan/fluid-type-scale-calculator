af52e9deb805c323ec0d5e334228917d479f8db6
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-12-29 07:38:53 -0500

    Fallback to just inter if google fonts API request fails

--- src/_data/googleFonts.js
+++ src/_data/googleFonts.js
@@ -1,14 +1,18 @@
 const Cache = require('@11ty/eleventy-cache-assets');
 
 const googleFonts = async () => {
-  const fonts = await Cache(
-    `https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&key=${process.env.GOOGLE_FONTS_API_KEY}`,
-    {
-      duration: '1d',
-      type: 'json',
-    }
-  );
-  return fonts.items.map((font) => font.family);
+  try {
+    const fonts = await Cache(
+      `https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&key=${process.env.GOOGLE_FONTS_API_KEY}`,
+      {
+        duration: '1d',
+        type: 'json',
+      }
+    );
+    return fonts.items.map((font) => font.family);
+  } catch (e) {
+    return ['Inter'];
+  }
 };
 
 module.exports = googleFonts;
