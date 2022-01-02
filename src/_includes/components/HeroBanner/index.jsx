b3c6de272ae0003204f3a57769e6b36592451f35
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-01-02 15:36:19 -0500

    Add jsdocs for new components

--- src/_includes/components/HeroBanner/index.jsx
+++ src/_includes/components/HeroBanner/index.jsx
@@ -1,6 +1,16 @@
 import styles from './styles.module.scss';
 
-const HeroBanner = ({ title, subtitle }) => {
+/**
+ * @typedef HeroBannerProps
+ * @property {string} title - the title of the hero banner
+ * @property {string} subtitle - the subtitle of the hero banner, to be displayed below the title
+ */
+
+/**
+ * @param {HeroBannerProps} props
+ */
+const HeroBanner = (props) => {
+  const { title, subtitle } = props;
   return (
     <header className={styles['hero-banner']}>
       <h1 className={styles.title}>{title}</h1>
