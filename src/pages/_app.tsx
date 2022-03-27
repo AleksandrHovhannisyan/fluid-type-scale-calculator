894244513b622ee135427622179726382ad5a7f7
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-03-27 16:10:29 -0400

    Update next home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../../styles/global.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,39 @@
 import type { NextPage } from 'next';
+import FluidTypeScaleCalculator from '../components/FluidTypeScaleCalculator/FluidTypeScaleCalculator';
+import GithubCorner from '../components/GithubCorner/GithubCorner';
+import HeroBanner from '../components/HeroBanner/HeroBanner';
+import Info from '../components/Info/Info';
 import { Layout } from '../components/Layout';
+import PageFooter from '../components/PageFooter/PageFooter';
+import { site } from '../data';
 
-const Home: NextPage = () => {
-  return <Layout></Layout>;
+// Fetch Google Fonts at build time
+export const getStaticProps = async () => {
+  try {
+    const response = await (
+      await fetch(`https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&key=${process.env.GOOGLE_FONTS_API_KEY}`)
+    ).json();
+    const fonts = response.items.map((font: { family: string }) => font.family);
+    return { props: { fonts } };
+  } catch (e) {
+    return { props: { fonts: ['Inter'] } };
+  }
+};
+
+type HomePageProps = {
+  fonts: string[];
+};
+
+const Home: NextPage<HomePageProps> = (props) => {
+  return (
+    <Layout>
+      <HeroBanner title={site.title} subtitle={site.description} />
+      <FluidTypeScaleCalculator fonts={props.fonts} />
+      <Info />
+      <PageFooter />
+      <GithubCorner />
+    </Layout>
+  );
 };
 
 export default Home;
