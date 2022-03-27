cf7635602c69bbfe457a8f6b8af7d75c81d69940
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-03-27 16:47:20 -0400

    Fix broken toAbsoluteUrl logic

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,10 @@
-import path from 'path';
 import { site } from './data';
 
 /** Prefixes the given relative url string with the base site URL. */
 export const toAbsoluteUrl = (url: string) => {
-  return path.join(site.url, url);
+  // Replace trailing slash, e.g., site.com/ => site.com
+  const siteUrl = site.url.replace(/\/$/, '');
+  // Replace starting slash, e.g., /path/ => path/
+  const relativeUrl = url.replace(/^\//, '');
+  return `${siteUrl}/${relativeUrl}`;
 };
