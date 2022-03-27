ec809f5320d500be6a8eedb5425eb967805f4efa
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-03-27 16:13:16 -0400

    Add barrel file for page components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as FluidTypeScaleCalculator } from './FluidTypeScaleCalculator/FluidTypeScaleCalculator';
+export { default as GithubCorner } from './GithubCorner/GithubCorner';
+export { default as HeroBanner } from './HeroBanner/HeroBanner';
+export { default as Info } from './Info/Info';
+export { default as PageFooter } from './PageFooter/PageFooter';
