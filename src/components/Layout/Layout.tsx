29739bbe211571081c93da20e19d19ec77494345
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-03-27 11:18:22 -0400

    Add favicons and json+ld

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,27 @@
 import { FC } from 'react';
+import { JsonLd } from 'jsonld/jsonld-spec';
 import Head from 'next/head';
 import SocialPreviewImage from '../../../public/assets/images/thumbnail.png';
-import { defaultSeoProps } from './Layout.constants';
+import { defaultSeoProps, faviconSizes } from './Layout.constants';
 import { LayoutProps } from './Layout.types';
 
 const Layout: FC<LayoutProps> = (props) => {
   const { seoProps = defaultSeoProps } = props;
+
+  const structuredData: JsonLd = {
+    '@context': 'http://schema.org',
+    '@type': 'WebApplication',
+    name: seoProps.title,
+    // TODO: url
+    url: '{{ site.url }}',
+    description: seoProps.description,
+    applicationCategory: 'DeveloperApplication',
+    genre: 'design',
+    browserRequirements: 'Requires JavaScript.',
+    softwareVersion: '1.0.0',
+    operatingSystem: 'All',
+  };
+
   return (
     <main id="page">
       <Head>
@@ -24,7 +40,10 @@ const Layout: FC<LayoutProps> = (props) => {
         <meta name="twitter:author" content="@hovhaDovah" />
         <meta name="twitter:description" content={seoProps.description} />
         <meta name="twitter:image" content={SocialPreviewImage.src} />
-        {/* TODO: favicon */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
+        {faviconSizes.map((width) => (
+          <link key={width} rel="icon" href={`/assets/images/favicon-${width}.png`} sizes={`${width}x${width}`}></link>
+        ))}
         <noscript>
           <style>{`#preview-inputs,#copy-to-clipboard-button{display:none}`}</style>
         </noscript>
