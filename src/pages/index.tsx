f57fb00a3a616a9c27754a9f19fa59f1aa0597b5
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-03-31 18:03:48 -0400

    Remove noscript banner and JS requirement (JSON-LD)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import type { GetStaticPropsResult, NextPage } from 'next';
-import Banner from '../components/Banner/Banner';
 import FluidTypeScaleCalculator from '../components/FluidTypeScaleCalculator/FluidTypeScaleCalculator';
-import { GITHUB_CORNER_SIZE } from '../components/GithubCorner/GithubCorner.constants';
 import HeroBanner from '../components/HeroBanner/HeroBanner';
 import Info from '../components/Info/Info';
 import Layout from '../components/Layout/Layout';
 import { site } from '../constants';
 import type { WithFonts } from '../types';
 import { getGoogleFontFamilies } from '../utils';
-import styles from './index.module.scss';
 
 type HomePageProps = WithFonts;
 
@@ -20,11 +17,6 @@ export const getStaticProps = async (): Promise<GetStaticPropsResult<HomePagePro
 const Home: NextPage<HomePageProps> = (props) => {
   return (
     <Layout>
-      <noscript>
-        <Banner className={styles['no-js-banner']} style={{ paddingInline: GITHUB_CORNER_SIZE }}>
-          This app requires JavaScript. Please enable it if you can.
-        </Banner>
-      </noscript>
       <HeroBanner title={site.title} subtitle={site.description} />
       <FluidTypeScaleCalculator fonts={props.fonts} />
       <Info />
