0d03eaa51412b7d6a8a830e4bea0189a6e6e2080
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-09 09:26:01 -0400

    Simplify DiscriminateUnion

--- src/types.generics.ts
+++ src/types.generics.ts
@@ -1,10 +1,10 @@
-/** Given a discriminated union of types `T`, returns the constituent that has `K` keys mapping to `V` values.
+/** Given a discriminated union of types `T`, extracts the constituent that has `K` keys mapping to `V` values.
  *
  * Docs: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#distributive-conditional-types
  *
  * Credit: https://stackoverflow.com/questions/50125893/typescript-derive-map-from-discriminated-union/50125960#50125960
  */
-export type DiscriminateUnion<T, K extends keyof T, V extends T[K]> = T extends Record<K, V> ? T : never;
+export type DiscriminateUnion<T, K extends keyof T, V extends T[K]> = Extract<T, Record<K, V>>;
 
 /** Given a type `T` of the shape `{ [K]: string }`, where `K` is a key of `T`, returns a mapped type
  * mapping each value under that key to the corresponding discriminated type/constituent from `T`.
