1f54e88fe24d5e0679b4962af4e4e2989878a2ab
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-09 09:59:58 -0400

    Write additional tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,5 +1,5 @@
 import { GOOGLE_FONTS_BASE_URL } from './constants';
-import { getGoogleFontLinkTagHref, toAbsoluteUrl } from './utils';
+import { getGoogleFontLinkTagHref, isNumber, throwIf, toAbsoluteUrl } from './utils';
 
 describe('App-wide utilities', () => {
   describe('toAbsoluteUrl', () => {
@@ -29,6 +29,36 @@ describe('App-wide utilities', () => {
       expect(getGoogleFontLinkTagHref({ family: 'Libre Baskerville', display: 'fallback' })).toStrictEqual(
         `${GOOGLE_FONTS_BASE_URL}?family=Libre+Baskerville&display=fallback`
       );
+    });
+  });
+  describe('isNumber', () => {
+    it('returns true for strings resembling numbers', () => {
+      expect(isNumber('9001')).toStrictEqual(true);
+      expect(isNumber('42.5')).toStrictEqual(true);
+      expect(isNumber('-12.0')).toStrictEqual(true);
+      expect(isNumber('0xfff')).toStrictEqual(true);
+    });
+    it('returns true for scientific notation strings', () => {
+      expect(isNumber('1e5')).toStrictEqual(true);
+    });
+    it('returns false for NaN', () => {
+      expect(isNumber(NaN)).toStrictEqual(false);
+    });
+    it('returns false for strings not resembling numbers', () => {
+      expect(isNumber('-12.f')).toStrictEqual(false);
+      expect(isNumber('hello world')).toStrictEqual(false);
+      expect(isNumber('0xzzz')).toStrictEqual(false);
+    });
+  });
+  describe('throwIf', () => {
+    it('does not throw if the condition is false', () => {
+      expect(() => throwIf(false)).not.toThrow();
+    });
+    it('throws if the condition is true', () => {
+      expect(() => throwIf(true)).toThrow();
+    });
+    it('throws the given error message', () => {
+      expect(() => throwIf(true, 'error message')).toThrowError({ name: 'Error', message: 'error message' });
     });
   });
 });
