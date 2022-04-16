b953c42f88663dcfb4f7f74cabe29a11ac8fae0d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-16 08:59:12 -0400

    Update comma-separated list regex to match prod

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ export const GOOGLE_FONTS_BASE_URL = `https://fonts.googleapis.com/css2`;
 /** The default font family used by the app and shown in font pickers. */
 export const DEFAULT_FONT_FAMILY = 'Inter';
 
-/** Regex to match a comma-separated of values, with only alphanumeric characters permitted between the commas. */
-export const COMMA_SEPARATED_LIST_REGEX = /^[a-zA-Z0-9-](?:(,\s*)?[a-zA-Z0-9-])*$/;
+/** Regex for a comma-separated list of word identifiers, with optional whitespace around the commas. */
+export const COMMA_SEPARATED_LIST_REGEX = /^[\w-]+(\s*,\s*[\w-]+)*$/;
 
 /** Enum of delays in milliseconds, for consistency across event handlers. */
 export enum Delay {
