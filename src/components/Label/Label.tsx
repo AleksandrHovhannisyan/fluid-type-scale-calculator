3171abb71f110666a6579186f9f81b5138c8371f
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-18 17:29:50 -0400

    Adjust spacing and fix Safari bug with fieldset

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -13,24 +13,19 @@ export type LabelProps = Omit<HTMLProps<HTMLLabelElement & HTMLFieldSetElement>,
   direction?: 'vertical' | 'horizontal';
 };
 
-const Label: FC<LabelProps> = ({
-  as: Tag = 'label',
-  className,
-  children,
-  title,
-  description,
-  direction = 'vertical',
-  ...otherProps
-}) => {
-  const labelText = (
-    <>
+const Label: FC<LabelProps> = (props) => {
+  const { as: Tag = 'label', className, children, title, description, direction = 'vertical', ...otherProps } = props;
+  const hasStylizedLabelText = !!title || !!description;
+
+  const labelText = hasStylizedLabelText && (
+    <span className={styles['label-text']}>
       {title && <span className={styles['label-title']}>{title}</span>}
       {description && <span className={styles['label-description']}>{description}</span>}
-    </>
+    </span>
   );
 
   return (
-    <Tag className={clsx(styles.label, className)} data-direction={direction} {...otherProps}>
+    <Tag className={clsx({ [styles.label]: Tag === 'label' }, className)} data-direction={direction} {...otherProps}>
       {Tag === 'fieldset' ? <legend>{labelText}</legend> : labelText}
       {children}
     </Tag>
