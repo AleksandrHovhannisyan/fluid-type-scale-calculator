11c6f44067f057918592d9a54d10bb4c3a0b86c2
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-19 17:49:12 -0400

    Hide RangeInput input label accessibly

--- src/components/RangeInput/RangeInput.tsx
+++ src/components/RangeInput/RangeInput.tsx
@@ -12,14 +12,15 @@ type RangeInputProps = Omit<InputProps, 'label' | 'type'> & {
  */
 const RangeInput = (props: RangeInputProps) => {
   const { id, label, required, ...otherProps } = props;
+  const numericInputId = `${id}-numeric`;
   return (
     <div className={styles.range}>
       <Label htmlFor={id} title={label} />
       <div className={styles['range-display']}>
         <Input type="range" id={id} step={1} {...otherProps} />
-        <Label direction="horizontal" title="Custom">
-          <Input type="number" step={1} delay={0} required={required} {...otherProps} />
-        </Label>
+        {/* Hide this label in an accessible manner since sighted users don't need to see it (the range label is sufficient). */}
+        <Label title="Custom screen width" className="sr-only" htmlFor={numericInputId} />
+        <Input id={numericInputId} type="number" step={1} delay={0} required={required} {...otherProps} />
       </div>
     </div>
   );
