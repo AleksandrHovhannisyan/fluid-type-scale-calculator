539df99ffaf797d4cab8b186a7dc0d51e068561f
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-01 18:06:29 -0400

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import ErrorPage from '../components/ErrorPage/ErrorPage';
+
+const Custom404Page = () => {
+  return <ErrorPage code={404} reasonPhrase="Not Found" description="The requested page could not be found." />;
+};
+
+export default Custom404Page;
