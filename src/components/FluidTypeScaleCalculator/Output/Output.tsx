87dc1c913932dfa9236951fd3a35706be314659a
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-10-18 06:35:34 -0400

    Clean up Output code indentation and add comments

--- src/components/FluidTypeScaleCalculator/Output/Output.tsx
+++ src/components/FluidTypeScaleCalculator/Output/Output.tsx
@@ -17,45 +17,54 @@ const Output = (props: Props) => {
   /** Helper to assemble a CSS custom property for a given font size step. */
   const getCustomPropertyName = (step: string) => `--${state.namingConvention}-${step}`;
 
-  const getFluidFontSizeVariables = (separator = '\n') =>
-    fontSizes
+  // We need the fluid variables in two scenarios, and in each scenario the indentation level is different
+  const getFluidFontSizeVariables = (indentLevel = 0) => {
+    const indentation = Array.from({ length: indentLevel }, () => '\t').join('');
+    return fontSizes
       .map(([step, { min, max, preferred }]) => {
-        return `${getCustomPropertyName(step)}: clamp(${min}, ${preferred}, ${max});`;
+        return `${indentation}${getCustomPropertyName(step)}: clamp(${min}, ${preferred}, ${max});`;
       })
-      .join(separator);
+      .join('\n')
+      .trim();
+  };
 
   let code: string | undefined;
 
   // Include fallbacks with feature queries for browsers that don't support clamp
   if (state.shouldIncludeFallbacks) {
+    const minFallbackVariables = fontSizes
+      .map(([step, { min }]) => {
+        return `\t\t${getCustomPropertyName(step)}: ${min};`;
+      })
+      .join('\n')
+      .trim();
+
+    const maxFallbackVariables = fontSizes
+      .map(([step, { max }]) => {
+        return `\t\t\t${getCustomPropertyName(step)}: ${max};`;
+      })
+      .join('\n')
+      .trim();
+
+    // Outdent to prevent the static code indentation from influencing the output string indentation
     code = outdent`
-      /* Fluid font size variables, for browsers that support clamp */
-      @supports (font-size: clamp(1rem, 1vw, 1rem)) {
+    /* Fluid font size variables, for browsers that support clamp */
+    @supports (font-size: clamp(1rem, 1vw, 1rem)) {
+      :root {
+        ${getFluidFontSizeVariables(2)}
+      }
+    }
+    /* Fallback variables for browsers that don't support clamp */
+    @supports not (font-size: clamp(1rem, 1vw, 1rem)) {
+      :root {
+        ${minFallbackVariables}
+      }
+      @media screen and (min-width: ${state.max.screenWidth}px) {
         :root {
-          ${getFluidFontSizeVariables('\n\t\t')}
+          ${maxFallbackVariables}
         }
       }
-      /* Fallback variables for browsers that don't support clamp */
-      @supports not (font-size: clamp(1rem, 1vw, 1rem)) {
-        :root {
-          ${fontSizes
-            .map(([step, { min }]) => {
-              return `\t${getCustomPropertyName(step)}: ${min};`;
-            })
-            // Extra tabs are needed for proper indentation
-            .join('\n\t\t')}
-        }
-        @media screen and (min-width: ${state.max.screenWidth}px) {
-          :root {
-            ${fontSizes
-              .map(([step, { max }]) => {
-                return `\t${getCustomPropertyName(step)}: ${max};`;
-              })
-              // Extra tabs are needed for proper indentation
-              .join('\n\t\t\t')}
-          }
-        }
-      }`;
+    }`;
   } else {
     code = getFluidFontSizeVariables();
   }
