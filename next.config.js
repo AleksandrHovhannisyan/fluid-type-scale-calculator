5fba8a38dbd118fec5ab7d8ceb3fbfc599989b5d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-05-09 18:45:06 -0500

    Use next config to set lang

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    defaultLocale: 'en-US',
+    locales: ['en-US'],
+  },
 };
 
 module.exports = nextConfig;
